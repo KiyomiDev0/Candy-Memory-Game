21dd3d70a7ad3da3d193d807a4b59aac6112516e
Kiyomi Dev <84685006+KiyomiDev@users.noreply.github.com>
2022-03-15 17:34:54 +0200

    Add alt attribute to images

--- script.js
+++ script.js
@@ -147,26 +147,26 @@ function ifAllCorrect() {
 // Generate Random Images
 function random() {
   let orderedArr = [
-    `<img src="imgs/1.png">`,
-    `<img src="imgs/2.png">`,
-    `<img src="imgs/3.png">`,
-    `<img src="imgs/4.png">`,
-    `<img src="imgs/5.png">`,
-    `<img src="imgs/6.png">`,
-    `<img src="imgs/7.png">`,
-    `<img src="imgs/8.png">`,
-    `<img src="imgs/9.png">`,
-    `<img src="imgs/10.png">`,
-    `<img src="imgs/1.png">`,
-    `<img src="imgs/2.png">`,
-    `<img src="imgs/3.png">`,
-    `<img src="imgs/4.png">`,
-    `<img src="imgs/5.png">`,
-    `<img src="imgs/6.png">`,
-    `<img src="imgs/7.png">`,
-    `<img src="imgs/8.png">`,
-    `<img src="imgs/9.png">`,
-    `<img src="imgs/10.png">`
+    `<img src="imgs/1.png" alt="candy-img">`,
+    `<img src="imgs/2.png" alt="candy-img">`,
+    `<img src="imgs/3.png" alt="candy-img">`,
+    `<img src="imgs/4.png" alt="candy-img">`,
+    `<img src="imgs/5.png" alt="candy-img">`,
+    `<img src="imgs/6.png" alt="candy-img">`,
+    `<img src="imgs/7.png" alt="candy-img">`,
+    `<img src="imgs/8.png" alt="candy-img">`,
+    `<img src="imgs/9.png" alt="candy-img">`,
+    `<img src="imgs/10.png" alt="candy-img">`,
+    `<img src="imgs/1.png" alt="candy-img">`,
+    `<img src="imgs/2.png" alt="candy-img">`,
+    `<img src="imgs/3.png" alt="candy-img">`,
+    `<img src="imgs/4.png" alt="candy-img">`,
+    `<img src="imgs/5.png" alt="candy-img">`,
+    `<img src="imgs/6.png" alt="candy-img">`,
+    `<img src="imgs/7.png" alt="candy-img">`,
+    `<img src="imgs/8.png" alt="candy-img">`,
+    `<img src="imgs/9.png" alt="candy-img">`,
+    `<img src="imgs/10.png" alt="candy-img">`
   ];
 
   function rand(max) {
